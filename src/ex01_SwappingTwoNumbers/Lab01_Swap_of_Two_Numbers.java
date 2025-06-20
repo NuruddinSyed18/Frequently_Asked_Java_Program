9c88b146941ca90dd605b8ba8a2e2cadcbd5d3e5
NuruddinSyed18 <nuruddinsyed18@gmail.com>
2025-06-20 08:05:51 +0530

    FAJP Practice 20th June

---
+++ src/ex01_SwappingTwoNumbers/Lab01_Swap_of_Two_Numbers.java
@@ -0,0 +1,7 @@
+package ex01_SwappingTwoNumbers;
+
+public class Lab01_Swap_of_Two_Numbers {
+    public static void main(String[] args){
+        System.out.println("Swap of two numbers");
+    }
+}
